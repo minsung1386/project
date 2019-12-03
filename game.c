161e41add3fb5ffae57c3941c767f21fdaa8a855
ploteen <ploteen@skku.edu>
2019-12-03 18:48:26 +0900

    [UPDATE] is_game_over function
    Signed-off-by: ploteen <ploteen@skku.edu>

--- game.c
+++ game.c
@@ -84,3 +84,24 @@ void new_num()
         }
     }
 }
+int is_game_over()
+{
+    int i, j;
+    //1. have an empty slot
+    for (i = 0; i < 4; i++)
+        for (j = 0; j < 4; j++)
+            if (board[i][j] == 0)
+                return 0;
+    //2. there are some data that can combine
+    for (i = 0; i < 3; i++)
+        for (j = 0; j < 3; j++)
+            if (board[i][j] == board[i + 1][j] || board[i][j] == board[i][j + 1])
+                return 0;
+    for (i = 0; i < 3; i++)
+        if (board[i][3] == board[i + 1][3])
+            return 0;
+    for (j = 0; j < 3; j++)
+        if (board[3][j] == board[3][j + 1])
+            return 0;
+    return 1;
+}

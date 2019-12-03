3354b2a5a6ed03d6b385bb28aaf6e81d31bad0c0
ploteen <ploteen@skku.edu>
2019-12-03 18:34:55 +0900

    [UPDATE]game.c : move, new_num function
    Signed-off-by: ploteen <ploteen@skku.edu>

--- game.c
+++ game.c
@@ -9,9 +9,8 @@
 #define RIGHT 77
 int board[4][4] = {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
 int score;
-void move(int key);
+void move();
 int is_game_over();
-void restart();
 void new_num();
 void start();
 void end();
@@ -22,6 +21,7 @@ void start()
     score = 0;
 
     draw_start(); //draw background
+    //draw new_num
     new_num();
     new_num();
     while (is_game_over() != 1)
@@ -33,8 +33,54 @@ void start()
         }
         else
         {
-            key == getch();
-            move(key);
+            move();
+        }
+    }
+    print_restart();
+    while (1)
+    {
+        switch (getch())
+        {
+        case 121:
+        case 89:
+            start();
+            break;
+        case 110:
+        case 78:
+        case ESC:
+            end();
+        }
+    }
+}
+
+void move(int key)
+{
+    int i, j;
+    int key = getch();
+    switch (key)
+    {
+    case LEFT:
+
+    case RIGHT:
+
+    case DOWN:
+
+    case UP:
+    }
+}
+
+void new_num()
+{
+    srand(time(NULL));
+    while (1)
+    {
+        int new_data = 2 * (rand() % 2 + 1);
+        int i = rand() % 3, j = rand() % 3;
+        if (board[i][j] == 0)
+        {
+            board[i][j] = new_data;
+            draw();
+            break;
         }
     }
 }

3657104d85062850699a20870a192d40715e72e8
ploteen <ploteen@skku.edu>
2019-12-03 18:01:38 +0900

    [UPDATE]game.c : start function
    Signed-off-by: ploteen <ploteen@skku.edu>

--- game.c
+++ game.c
@@ -2,11 +2,39 @@
 #include <time.h>
 #include <windows.h>
 
+#define ESC 27
+#define LEFT 75
+#define UP 72
+#define DOWN 80
+#define RIGHT 77
 int board[4][4] = {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
-
-void move();
-void is_game_over();
+int score;
+void move(int key);
+int is_game_over();
 void restart();
 void new_num();
 void start();
 void end();
+void get_key();
+
+void start()
+{
+    score = 0;
+
+    draw_start(); //draw background
+    new_num();
+    new_num();
+    while (is_game_over() != 1)
+    {
+        int key = getch();
+        if (key == ESC)
+        {
+            end();
+        }
+        else
+        {
+            key == getch();
+            move(key);
+        }
+    }
+}

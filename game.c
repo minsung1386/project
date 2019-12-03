1a7cf51375b7f60f0f22149548afd3ecf545368a
ploteen <ploteen@skku.edu>
2019-12-03 16:52:54 +0900

    make skeleton
    Signed-off-by: ploteen <ploteen@skku.edu>

---
+++ game.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <time.h>
+#include <windows.h>
+
+int board[4][4] = {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
+
+void move();
+void is_game_over();
+void restart();
+void new_num();
+void start();

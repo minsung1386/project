21c34039798399e8a57dfc7e3913e1745a3a5931
ploteen <ploteen@skku.edu>
2019-12-04 18:32:36 +0900

    [FIX]game.c, game.h

--- game.h
+++ game.h
@@ -8,10 +8,12 @@
 #define RIGHT 77
 
 extern int board[4][4] = {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
-extern int score;
+extern int cur_score;
 
 void start(void);
 void move(void);
 int is_game_over(void);
 void new_num(void);
 void end(void);
+
+#endif

07a43808d84c1df877bdda0811ce467411938f6f
minsung1386 <50101334+minsung1386@users.noreply.github.com>
2019-12-13 21:21:07 +0900

    [UPDATE]compile ok(exclude game.c file from build)

--- game.h
+++ game.h
@@ -3,15 +3,18 @@
 
 #define ESC 27
 #define Q 113
-#define W 119
+//#define W 119
+#define W 72
 #define E 101
-#define A 97
-#define D 100
+//#define A 97
+#define A 75
+//#define D 100
+#define D 77
 #define Z 122
-#define X 120
+//#define X 120
+#define X 80
 #define C 99
 
-extern int board[4][4] = {{0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}, {0, 0, 0, 0}};
 extern int cur_score;
 
 void start(void);
